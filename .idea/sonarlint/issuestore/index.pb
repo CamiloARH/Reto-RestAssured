f2fb7ed8a976088cad38caee1c5cde7f28e5c2a0
Camilo A.R.H <cuadrisama@hotmail.com>
2021-12-30 21:44:34 -0500

    Se añade el plan de pruebas y algunas modificaciones menores al programa principal

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
+n
+>src/test/java/co/com/sofka/runner/RetoCucumberRestAssured.java,8\6\869515d694c8840427e41de5d964858428e79504
+t
+Dsrc/test/java/co/com/sofka/stepdefinition/GetAndPatchDefinition.java,5\4\544841fea18e5acdedebe01042512ab65ce3b321
+g
+7src/test/java/co/com/sofka/stepdefinition/Reqresin.java,6\4\6400afef3f31e2dffc772718bbbba404a02ffa3a
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
--- src/test/java/co/com/sofka/stepdefinition/GetAndPatchDefinition.java
+++ src/test/java/co/com/sofka/stepdefinition/GetAndPatchDefinition.java
@@ -1,27 +1,30 @@
 package co.com.sofka.stepdefinition;
 
+import co.com.sofka.runner.RetoCucumberRestAssured;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Assertions;
 
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
+
 public class GetAndPatchDefinition extends Reqresin{
 
-    private RequestSpecification request;
     private Response response;
-
+    private static final Logger LOGGER = Logger.getLogger(RetoCucumberRestAssured.class);
     //Scenario 1
     @Given("^el usuario esta en la pagina de busqueda y desea buscar en la lista de recursos$")
     public void elUsuarioEstaEnLaPaginaDeBusquedaYDeseaBuscarEnLaListaDeRecursos() {
         try {
             generalSteUp();
+            LOGGER.info("Se organiza el setUp inicial");
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el Give");
             Assertions.fail(e.getMessage());
         }
     }
@@ -36,11 +39,10 @@ public class GetAndPatchDefinition extends Reqresin{
                 response = given()
                         .get(RESOURCE_UNKNOWN_NOT_FOUND);
             }else{
-                ////log4j
-                System.out.println("Fallo 1");
+                LOGGER.info("No es ninguna de las esperadas");
             }
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el when");
             Assertions.fail(e.getMessage());
         }
     }
@@ -53,12 +55,11 @@ public class GetAndPatchDefinition extends Reqresin{
             }else if(name.equals("null")){
                 response.then().statusCode(HttpStatus.SC_NOT_FOUND);
             }else {
-                ////log4j
-                System.out.println("Fallo 2");
+                LOGGER.info("No es ninguna de las esperadas");
             }
 
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el then");
             Assertions.fail(e.getMessage());
         }
     }
@@ -68,9 +69,9 @@ public class GetAndPatchDefinition extends Reqresin{
     public void elUsuarioEstaEnLaPaginaDeUpdateDeUsuarios() {
         try {
             generalSteUp();
-
+            LOGGER.info("Se organiza el setUp inicial");
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el Give");
             Assertions.fail(e.getMessage());
         }
     }
@@ -78,15 +79,15 @@ public class GetAndPatchDefinition extends Reqresin{
     @When("el usuario escribe el nombre {string} y cambia su cargo {string}")
     public void elUsuarioEscribeElNombreYCambiaSuCargo(String name, String job) {
         try {
-            request = given().body("{\n" +
-                "    \"name\": \""+name+"\",\n" +
-                "    \"job\": \""+job+"\"\n" +
-                "}");
+            RequestSpecification request = given().body("{\n" +
+                    "    \"name\": \"" + name + "\",\n" +
+                    "    \"job\": \"" + job + "\"\n" +
+                    "}");
             response = request
                     .patch(RESOURCE_USERS);
 
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el when");
             Assertions.fail(e.getMessage());
         }
     }
@@ -100,7 +101,7 @@ public class GetAndPatchDefinition extends Reqresin{
                     .body("updatedAt", notNullValue());
 
         } catch (Exception e) {
-            //log4j
+            LOGGER.error("Problema en el then");
             Assertions.fail(e.getMessage());
         }
     }
